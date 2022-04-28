e1131e1bf9e3e3081be6f13771d985d158cb058c
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-28 16:34:26 +0200

    aggiunto il thumb

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,6 +1,16 @@
 @extends('template.layout')
 @section('content')
 <div id="comic">
+    <div id="topbar">
+        <div id="container">
+            <div>
+                <img src="{{$comic['thumb']}}" alt="">
+                <div>
+                    <a href="">VIEW GALLERY</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div id="info">
             <h2>{{$comic['title']}}</h2>
