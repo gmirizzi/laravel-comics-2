02befa9a8dea686cf2ea31a256c92082b903cd0a
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-28 18:27:21 +0200

    aggiunte le specs, l'adv e modificato lo stile

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,28 +4,88 @@
     <div id="topbar">
         <div id="container">
             <div>
-                <img src="{{$comic['thumb']}}" alt="">
                 <div>
-                    <a href="">VIEW GALLERY</a>
+                    <img src="{{$comic['thumb']}}" alt="">
                 </div>
+                    <a href="#">VIEW GALLERY</a>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div id="info">
-            <h2>{{$comic['title']}}</h2>
-            <div id="price">
-                <div>U.S. Price: {{$comic['price']}}</div>
-                <div>
-                    <span>AVAILABLE</span>
-                    <button>Check Availability</button>
+    <div id="maincontent">
+        <div class="container">
+            <div id="info">
+                <h2>{{$comic['title']}}</h2>
+                <div id="price">
+                    <div>
+                        <span>U.S. Price: </span>{{$comic['price']}}</div>
+                    <div>
+                        <span>AVAILABLE</span>
+                        <button>Check Availability</button>
+                    </div>
                 </div>
+                <p>{{$comic['description']}}</p>
             </div>
-            <p>{{$comic['description']}}</p>
+            <div id="adv">
+                <div>ADVERTISEMENT</div>
+                <img src="{{asset('images/adv.jpg')}}" alt="adv">
+            </div>
         </div>
-        <div id="adv">
-
+    </div>
+    <div id="bottom">
+        <div class="container">
+            <div id="talent">
+                <h3>Talent</h3>
+                <hr>
+                <div class="row">
+                    <div class="title">
+                        Art by:
+                    </div>
+                    <div class="content">
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="title">
+                        Written by:
+                    </div>
+                    <div class="content">
+                    </div>
+                </div>
+                <hr>
+                <div class="row"></div>
+            </div>
+            <div id="specs">
+                <h3>Specs</h3>
+                <hr>
+                <div class="row">
+                    <div class="title">
+                        Series:
+                    </div>
+                    <div class="content">
+                        {{$comic['series']}}
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="title">
+                        U.S. Price:
+                    </div>
+                    <div class="content">
+                        {{$comic['price']}}
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="title">
+                        On Sale Date:
+                    </div>
+                    <div class="content">
+                        {{$comic['sale_date']}}
+                    </div>
+                </div>
+                <hr>
+            </div>
         </div>
-    </div>    
+    </div>   
 </div>
 @endsection
