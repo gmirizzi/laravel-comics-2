f60afe891532dbc6ae2270a07e366422d113ccb5
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-28 16:01:58 +0200

    inserito titolo descrizione e prezzo

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,1 +1,21 @@
 @extends('template.layout')
+@section('content')
+<div id="comic">
+    <div class="container">
+        <div id="info">
+            <h2>{{$comic['title']}}</h2>
+            <div id="price">
+                <div>U.S. Price: {{$comic['price']}}</div>
+                <div>
+                    <span>AVAILABLE</span>
+                    <button>Check Availability</button>
+                </div>
+            </div>
+            <p>{{$comic['description']}}</p>
+        </div>
+        <div id="adv">
+
+        </div>
+    </div>    
+</div>
+@endsection
